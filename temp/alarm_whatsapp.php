4a7d515434233b6f1020e714a4c3659bc8fdd93b
marcosasilvalepe <61514719+marcosasilvalepe@users.noreply.github.com>
2023-10-08 11:13:41 -0300

    Update alarm_whatsapp.php

--- temp/alarm_whatsapp.php
+++ temp/alarm_whatsapp.php
@@ -8,8 +8,8 @@
 
 	// Find your Account Sid and Auth Token at twilio.com/console
 	// and set the environment variables. See http://twil.io/secure
-	$sid = getenv("ACd6db45eba7ca8b1e522992298182b453");
-	$token = getenv("8acc55cb5775aa2e8c3acca43cacae8c");
+	$sid = getenv("twilio id");
+	$token = getenv("twilio token");
 	$twilio = new Client($sid, $token);
 
 	$message = $twilio->messages
